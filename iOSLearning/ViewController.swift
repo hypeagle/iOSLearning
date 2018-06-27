5603239124a4749c1958d9a3df9f276b6d77ce07
hypeagle <554616332@qq.com>
2018-06-27 14:59:35 +0800

    Add location.

--- iOSLearning/ViewController.swift
+++ iOSLearning/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+    
+    @IBAction func startLocate(_ sender: UIButton) {
+        locateMap()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locateMap() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 5.0
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("定位失败")
+        
+        let alter = UIAlertController(title: "提示", message: "请在设置中打开定位", preferredStyle: UIAlertControllerStyle.alert)
+        let ok = UIAlertAction(title: "打开定位", style: UIAlertActionStyle.default) { (_) in
+            let url = URL(fileURLWithPath: UIApplicationOpenSettingsURLString)
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        let cancel = UIAlertAction(title: "取消", style: UIAlertActionStyle.cancel, handler: nil)
+        alter.addAction(cancel)
+        alter.addAction(ok)
+        self.present(alter, animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        print("定位成功")
+        
+        if let location = locations.last {
+            print(location.coordinate.latitude, location.coordinate.longitude)
+            
+            let locationAge = -location.timestamp.timeIntervalSinceNow
+            print(locationAge)
+            
+            let geo = CLGeocoder()
+            geo.reverseGeocodeLocation(location) { (placeMarks, error) in
+                if let placeMark = placeMarks?.first {
+                    let str: String = "当前国家：\(placeMark.country ?? "")\n当前城市：\(placeMark.locality ?? "")\n当前位置：\(placeMark.subLocality ?? "")\n当前街道：\(placeMark.thoroughfare ?? "")\n具体位置：\(placeMark.name ?? "")"
+                    print(str)
+                    let alter = UIAlertController(title: "定位成功", message: str, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "确定", style: UIAlertActionStyle.cancel, handler: nil)
+                    alter.addAction(ok)
+                    self.present(alter, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
 
